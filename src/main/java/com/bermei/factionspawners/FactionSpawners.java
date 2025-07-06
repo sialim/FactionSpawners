468b5ed188131ad0bea053f980b4cc431851e115
sialim <syystonttu@gmail.com>
2025-07-05 19:24:02 -0700

    Fixed file creation

--- src/main/java/com/bermei/factionspawners/FactionSpawners.java
+++ src/main/java/com/bermei/factionspawners/FactionSpawners.java
@@ -3,6 +3,7 @@ package com.bermei.factionspawners;
 import com.bermei.factionspawners.commands.FactionCommand;
 import com.bermei.factionspawners.events.ChatToSignListener;
 import com.bermei.factionspawners.events.ClickGUI;
+import com.bermei.factionspawners.events.ProtectionListener;
 import com.bermei.factionspawners.factions.FactionManager;
 import com.bermei.factionspawners.utilities.ActionBarMessages;
 import com.bermei.factionspawners.utilities.InviteStorage;
@@ -16,20 +17,26 @@ public final class FactionSpawners extends JavaPlugin {
     public FactionCommand factionCommand;
     public InviteStorage invites;
     public ClickGUI clickListener;
+    public ProtectionListener protectionListener;
 
     @Override
     public void onEnable() {
+        invites = new InviteStorage();
         signChat = new ChatToSignListener();
         factionManager = new FactionManager(this);
         util = new ActionBarMessages();
         factionCommand = new FactionCommand(this);
-        invites = new InviteStorage();
         clickListener = new ClickGUI(this);
+        protectionListener = new ProtectionListener();
 
         getCommand("f").setExecutor(factionCommand);
         getCommand("f").setTabCompleter(factionCommand);
 
         Bukkit.getPluginManager().registerEvents(signChat, this);
+        Bukkit.getPluginManager().registerEvents(clickListener, this);
+        Bukkit.getPluginManager().registerEvents(protectionListener, this);
+
+        saveDefaultConfig();
     }
 
     @Override
